edeb8981b69af0b71ed769432c73dab49785442f
unknown <mauricio_lopezs@hotmail.com>
2021-01-15 11:22:44 -0600

    update-15

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { } from 'react-bootstrap';
+
+const Header = () => {
+  return ( 
+
+   );
+}
+ 
+export default Header;
